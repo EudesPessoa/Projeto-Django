851e83c8cc29a61a6a42797e8975cb778b0c7bc2
EudesPessoa <eudespessoaitu@gmail.com>
2023-07-28 14:37:31 -0300

    atualizando

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views # Esse ponto quer dizer da mesma pasta
+
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home), # Home
+    path('', views.home, name='home'), # Home
+    path('recipes/<int:id>/', views.recipe, name='recipe'), 
 
 
 ]
